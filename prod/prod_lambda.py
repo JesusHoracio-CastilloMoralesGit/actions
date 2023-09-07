312f110f5201c4f2e4a995bbc16922d530025f33
JesusHoracio-CastilloMoralesGit <jesus.castillomorales07@gmail.com>
2023-09-07 02:40:23 -0600

    initial commit

---
+++ prod/prod_lambda.py
@@ -0,0 +1,14 @@
+import json
+import datetime
+import requests
+def lambda_handler(event, context):
+    print(event)
+    data = {
+        'output': 'hello from ' + event[('country\n')],
+        'timestamp': datetime.datetime.utcnow().isofromat()
+    }
+    return {'statuscode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}
+            }
+    
