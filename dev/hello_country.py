73b0f0d6c4e5163d68e1c5b0e71a3cbe66115aa1
JesusHoracio-CastilloMoralesGit <jesus.castillomorales07@gmail.com>
2023-09-07 15:39:26 -0600

    adios

--- dev/hello_country.py
+++ dev/hello_country.py
@@ -1,6 +1,5 @@
 import json
 import datetime
-import requests
 def lambda_handler(event, context):
     print(event)
     data = {
--- prod/prod_lambda.py
+++ prod/prod_lambda.py
@@ -1,14 +1,13 @@
-# import json
-# import datetime
-# import requests
-# def lambda_handler(event, context):
-#     print(event)
-#     data = {
-#         'output': 'hello from ' + event[('country\n')],
-#         'timestamp': datetime.datetime.utcnow().isofromat()
-#     }
-#     return {'statuscode': 200,
-#             'body': json.dumps(data),
-#             'headers': {'Content-Type': 'application/json'}
-#             }
+import json
+import datetime
+def lambda_handler(event, context):
+    print(event)
+    data = {
+        'output': 'hello from ' + event[('country\n')],
+        'timestamp': datetime.datetime.utcnow().isofromat()
+    }
+    return {'statuscode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}
+            }
     
